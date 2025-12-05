c3b97a599c751dcd85a17a25a06f8fa2ecf284ef
towsatt <leonardo.bilato03@gmail.com>
2025-12-05 14:58:51 +0100

    database

---
+++ sito/database.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE users (
+    nome_utente    VARCHAR(50) PRIMARY KEY,
+    email          VARCHAR(255) NOT NULL UNIQUE,
+    password       VARCHAR(255) NOT NULL,
+    descrizione    TEXT
+);
+
+
+CREATE TABLE campagna (
+    id_campagna     SERIAL PRIMARY KEY,
+    descrizione     TEXT,
+    dungeon_master  INTEGER NOT NULL REFERENCES users(id_user),
+    pubblica        BOOLEAN DEFAULT FALSE
+);
+
+
+CREATE TABLE partecipanti (
+    id_campagna  INTEGER NOT NULL REFERENCES campagna(id_campagna) ON DELETE CASCADE,
+    id_user      INTEGER NOT NULL REFERENCES users(id_user)        ON DELETE CASCADE,
+    PRIMARY KEY (id_campagna, id_user)
+);
+
+
+CREATE TABLE sessione (
+    id_campagna  INTEGER NOT NULL REFERENCES campagna(id_campagna) ON DELETE CASCADE,
+    id_sessione  SERIAL,
+    riassunto    TEXT,
+    PRIMARY KEY (id_campagna, id_sessione)
+);
+
+
+CREATE TABLE logbook (
+    id_campagna   INTEGER NOT NULL REFERENCES campagna(id_campagna) ON DELETE CASCADE,
+    id_sessione   INTEGER NOT NULL,
+    id_user       INTEGER NOT NULL REFERENCES users(id_user)        ON DELETE CASCADE,
+    descr_pubblica  TEXT,
+    descr_privata   TEXT,
+    PRIMARY KEY (id_campagna, id_sessione, id_user),
+    FOREIGN KEY (id_campagna, id_sessione)
+        REFERENCES sessione(id_campagna, id_sessione)
+        ON DELETE CASCADE
+);
