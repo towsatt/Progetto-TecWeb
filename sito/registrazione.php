f7a25eee1ea98953b8ac9537175ec93cb98a0ca7
Arianna <161738321+MissAssaggini@users.noreply.github.com>
2026-06-05 17:03:13 +0200

    Modifiche login reg (#10)
    * Implementazione della validazione per email e username esistenti nella registrazione e login
    
    * Aggiunta della verifica della password durante la ricerca dell'utente

--- sito/registrazione.php
+++ sito/registrazione.php
@@ -1,51 +1,32 @@
 <?php
-include './html/registrazione.html';
-include 'connessione_DB.php';
+include_once __DIR__ . '/html/registrazione.html';
+require_once __DIR__ . '/../connessione_DB.php';
+include_once __DIR__ . '/PHP/Handlers/ErrorHandler.php';
+include_once __DIR__ . '/PHP/Controllers/InputController.php';
 
-$message = "";
-$toastClass = "";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirm_password'];
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    global $conn;
+    $username = InputController::validateUsername($_POST['username']);
+    $email = InputController::validateEmail($_POST['email']);
+    $password = InputController::validatePassword($_POST['password']);
+    $confirmPassword = InputController::validatePassword($_POST['confirm_password']);
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Check if email already exists
-    $checkEmailStmt = $conn->prepare("SELECT email FROM userdata WHERE email = ?");
-    $checkEmailStmt->bind_param("s", $email);
-    $checkEmailStmt->execute();
-    $checkEmailStmt->store_result();
-    $checkUserStmt = $conn->prepare("SELECT username FROM userdata WHERE username = ?");
-    $checkUserStmt->bind_param("s", $username);
-    $checkUserStmt->execute();
-    $checkUserStmt->store_result();
-
-    
-    if(!empty($username) && !empty($password) && !empty($confirmPassword) && !empty($email)) {
-    // Il messaggio di errore deve vedersi tramite CSS nascosto    
     if ($password !== $confirmPassword) {
-            $message = "Passwords do not match";
-        } else if ($checkEmailStmt->num_rows > 0) {
-            $message = "Email ID already exists";
-        } else if($checkUserStmt->num_rows > 0) {
-            $message = "Username already exists";
-        } else {
-            $stmt = $conn->prepare("INSERT INTO userdata (username, email, password) VALUES (?, ?, ?)");
-            $stmt->bind_param("sss", $username, $email, $hashedPassword);
-            if ($stmt->execute()) {
-                $message = "Account created successfully";
-            } else {
-                $message = "Error: " . $stmt->error;
-            }
-            $stmt->close();
-        }
-    } else {
-        $message = "Some fields are empty";
+        throw new InputError("Le password non coincidono.");
     }
 
-    $checkEmailStmt->close();
-    $conn->close();
+    //controlla se email e username sono già in uso
+    InputValidator::findMatchEmail($email);
+    InputValidator::findMatchUsername($username);
+    
+    //inserimento dati nel database
+    $stmt = $conn->prepare("INSERT INTO userdata (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $hashedPassword);
+    $stmt->execute();
+    $stmt->close();
+
+    
 }
 ?>
